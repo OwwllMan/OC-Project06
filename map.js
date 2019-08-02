6f345e28816cee2c3d69a229862f6c4b3492aa67
Mathieu D <bonjour@spectral-agency.com>
2019-08-02 19:47:53 +0200

    First push with the map algo

---
+++ map.js
@@ -0,0 +1,254 @@
+// var h = document.head;// Contient de head du DOM 
+// console.log(h)
+
+// var b = document.body;// Contient de head du DOM 
+// console.log(b)
+
+
+
+
+// child = b.childNodes[0];
+// console.log(child);
+// console.log(document.body.childNodes[0]); //Récupération de l'enfant 1 du body
+
+
+// for (var i = 0; i < document.body.childNodes.length; i++){
+//	 console.log(document.body.childNodes[i]);
+// }
+
+// var h1 = document.body.childNodes[1];
+// console.log(h1.parentNode); // Affiche le noeud parent 
+
+var i1 = 0;
+var i2 = 0;
+var i3 = 0;
+var i4 = 0;
+var i5 = 0;
+var i6 = 0;
+var i7 = 0;
+var i8 = 0;
+var i9 = 0;
+var i10 = 0;
+
+
+
+
+
+
+var num = 0; // var num = 0;
+
+
+
+nbRocheCol1  = 0;
+while (i1 < 10) {
+	if (nbRocheCol1 < 2){
+		i1ran = Math.floor(Math.random() * 2);
+		if (i1ran === 0){
+		document.getElementById("col1").innerHTML += `<img id="Col1case${num}" class="column1" src="pixil-frame-0.png" alt="Image Terre"  height="60" width="60">`;
+		i1 ++
+		num ++
+		}else{
+		document.getElementById("col1").innerHTML += `<img id="Col1case${num}" class="column1" src="pixil-frame-0-1.png" alt="Image Terre"  height="60" width="60">`;
+		nbRocheCol1 ++
+		i1 ++
+		num ++
+		}
+	}else{
+		document.getElementById("col1").innerHTML += `<img id="Col1case${num}" class="column1" src="pixil-frame-0.png" alt="Image Terre"  height="60" width="60">`;
+		i1 ++
+		num ++
+	}
+}
+
+nbRocheCol2  = 0;
+while (i2 < 10) {
+	if (nbRocheCol2 < 2){
+		i2ran = Math.floor(Math.random() * 2);
+		if (i2ran === 0){
+		document.getElementById("col2").innerHTML += `<img id="Col2case${num}" class="column2" src="pixil-frame-0.png" alt="Image Terre"  height="60" width="60">`;
+		i2 ++
+		num ++
+		}else{
+		document.getElementById("col2").innerHTML += `<img id="Col2case${num}" class="column2" src="pixil-frame-0-1.png" alt="Image Terre"  height="60" width="60">`;
+		nbRocheCol2 ++
+		i2 ++
+		num ++
+		}
+	}else{
+		document.getElementById("col2").innerHTML += `<img id="Col2case${num}" class="column2" src="pixil-frame-0.png" alt="Image Terre"  height="60" width="60">`;
+		i2 ++
+		num ++
+	}
+}
+
+nbRocheCol3  = 0;
+while (i3 < 10) {
+	if (nbRocheCol3 < 2){
+		i3ran = Math.floor(Math.random() * 2);
+		if (i3ran === 0){
+		document.getElementById("col3").innerHTML += `<img id="Col3case${num}" class="column3" src="pixil-frame-0.png" alt="Image Terre"  height="60" width="60">`;
+		i3 ++
+		num ++
+		}else{
+		document.getElementById("col3").innerHTML += `<img id="Col3case${num}" class="column3" src="pixil-frame-0-1.png" alt="Image Terre"  height="60" width="60">`;
+		nbRocheCol3 ++
+		i3 ++
+		num ++
+		}
+	}else{
+		document.getElementById("col3").innerHTML += `<img id="Col3case${num}" class="column3" src="pixil-frame-0.png" alt="Image Terre"  height="60" width="60">`;
+		i3 ++
+		num ++
+	}
+}
+
+nbRocheCol4  = 0;
+while (i4 < 10) {
+	if (nbRocheCol4 < 2){
+		i4ran = Math.floor(Math.random() * 2);
+		if (i4ran === 0){
+		document.getElementById("col4").innerHTML += `<img id="Col4case${num}" class="column4" src="pixil-frame-0.png" alt="Image Terre"  height="60" width="60">`;
+		i4 ++
+		num ++
+		}else{
+		document.getElementById("col4").innerHTML += `<img id="Col4case${num}" class="column4" src="pixil-frame-0-1.png" alt="Image Terre"  height="60" width="60">`;
+		nbRocheCol4 ++
+		i4 ++
+		num ++
+		}
+	}else{
+		document.getElementById("col4").innerHTML += `<img id="Col4case${num}" class="column4" src="pixil-frame-0.png" alt="Image Terre"  height="60" width="60">`;
+		i4 ++
+		num ++
+	}
+}
+
+nbRocheCol5  = 0;
+while (i5 < 10) {
+	if (nbRocheCol5 < 2){
+		i5ran = Math.floor(Math.random() * 2);
+		if (i5ran === 0){
+		document.getElementById("col5").innerHTML += `<img id="Col5case${num}" class="column5" src="pixil-frame-0.png" alt="Image Terre"  height="60" width="60">`;
+		i5 ++
+		num ++
+		}else{
+		document.getElementById("col5").innerHTML += `<img id="Col5case${num}" class="column5" src="pixil-frame-0-1.png" alt="Image Terre"  height="60" width="60">`;
+		nbRocheCol5 ++
+		i5 ++
+		num ++
+		}
+	}else{
+		document.getElementById("col5").innerHTML += `<img id="Col5case${num}" class="column5" src="pixil-frame-0.png" alt="Image Terre"  height="60" width="60">`;
+		i5 ++
+		num ++
+	}
+}
+
+nbRocheCol6  = 0;
+while (i6 < 10) {
+	if (nbRocheCol6 < 2){
+		i6ran = Math.floor(Math.random() * 2);
+		if (i6ran === 0){
+		document.getElementById("col6").innerHTML += `<img id="Col6case${num}" class="column6" src="pixil-frame-0.png" alt="Image Terre"  height="60" width="60">`;
+		i6 ++
+		num ++
+		}else{
+		document.getElementById("col6").innerHTML += `<img id="Col6case${num}" class="column6" src="pixil-frame-0-1.png" alt="Image Terre"  height="60" width="60">`;
+		nbRocheCol6 ++
+		i6 ++
+		num ++
+		}
+	}else{
+		document.getElementById("col6").innerHTML += `<img id="Col6case${num}" class="column6" src="pixil-frame-0.png" alt="Image Terre"  height="60" width="60">`;
+		i6 ++
+		num ++
+	}
+}
+
+nbRocheCol7  = 0;
+while (i7 < 10) {
+	if (nbRocheCol7 < 2){
+		i7ran = Math.floor(Math.random() * 2);
+		if (i7ran === 0){
+		document.getElementById("col7").innerHTML += `<img id="Col7case${num}" class="column7" src="pixil-frame-0.png" alt="Image Terre"  height="60" width="60">`;
+		i7 ++
+		num ++
+		}else{
+		document.getElementById("col7").innerHTML += `<img id="Col7case${num}" class="column7" src="pixil-frame-0-1.png" alt="Image Terre"  height="60" width="60">`;
+		nbRocheCol7 ++
+		i7 ++
+		num ++
+		}
+	}else{
+		document.getElementById("col7").innerHTML += `<img id="Col7case${num}" class="column7" src="pixil-frame-0.png" alt="Image Terre"  height="60" width="60">`;
+		i7 ++
+		num ++
+	}
+}
+
+nbRocheCol8  = 0;
+while (i8 < 10) {
+	if (nbRocheCol8 < 2){
+		i8ran = Math.floor(Math.random() * 2);
+		if (i8ran === 0){
+		document.getElementById("col8").innerHTML += `<img id="Col8case${num}" class="column8" src="pixil-frame-0.png" alt="Image Terre"  height="60" width="60">`;
+		i8 ++
+		num ++
+		}else{
+		document.getElementById("col8").innerHTML += `<img id="Col8case${num}" class="column8" src="pixil-frame-0-1.png" alt="Image Terre"  height="60" width="60">`;
+		nbRocheCol8 ++
+		i8 ++
+		num ++
+		}
+	}else{
+		document.getElementById("col8").innerHTML += `<img id="Col8case${num}" class="column8" src="pixil-frame-0.png" alt="Image Terre"  height="60" width="60">`;
+		i8 ++
+		num ++
+	}
+}
+
+nbRocheCol9  = 0;
+while (i9 < 10) {
+	if (nbRocheCol9 < 2){
+		i9ran = Math.floor(Math.random() * 2);
+		if (i9ran === 0){
+		document.getElementById("col9").innerHTML += `<img id="Col9case${num}" class="column9" src="pixil-frame-0.png" alt="Image Terre"  height="60" width="60">`;
+		i9 ++
+		num ++
+		}else{
+		document.getElementById("col9").innerHTML += `<img id="Col9case${num}" class="column9" src="pixil-frame-0-1.png" alt="Image Terre"  height="60" width="60">`;
+		nbRocheCol9 ++
+		i9 ++
+		num ++
+		}
+	}else{
+		document.getElementById("col9").innerHTML += `<img id="Col9case${num}" class="column9" src="pixil-frame-0.png" alt="Image Terre"  height="60" width="60">`;
+		i9 ++
+		num ++
+	}
+}
+
+nbRocheCol10  = 0;
+while (i10 < 10) {
+	if (nbRocheCol10 < 2){
+		i10ran = Math.floor(Math.random() * 2);
+		if (i10ran === 0){
+		document.getElementById("col10").innerHTML += `<img id="Col10case${num}" class="column10" src="pixil-frame-0.png" alt="Image Terre"  height="60" width="60">`;
+		i10 ++
+		num ++
+		}else{
+		document.getElementById("col10").innerHTML += `<img id="Col10case${num}" class="column10" src="pixil-frame-0-1.png" alt="Image Terre"  height="60" width="60">`;
+		nbRocheCol10 ++
+		i10 ++
+		num ++
+		}
+	}else{
+		document.getElementById("col10").innerHTML += `<img id="Col10case${num}" class="column10" src="pixil-frame-0.png" alt="Image Terre"  height="60" width="60">`;
+		i10 ++
+		num ++
+	}
+}
+
+
+
+
